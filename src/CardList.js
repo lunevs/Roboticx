d117d053f23a6e39bcdb882b9bfde69612906a3d
Sergey Lunev <lunev.s@gmail.com>
2020-04-14 15:45:46 +0300

    adding card list

---
+++ src/CardList.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Card from "./Card";
+
+
+const CardList = ({robots}) => {
+    return (
+        <div>
+            {robots.map(item => <Card key={item.id} id={item.id} name={item.name} email={item.email} />)}
+        </div>
+    );
+}
+
+export default CardList;
