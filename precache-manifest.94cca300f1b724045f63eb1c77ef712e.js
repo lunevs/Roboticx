de8b09135126fa6397e2c12f5654ec9b7fc024af
Sergey Lunev <lunev.s@gmail.com>
2020-04-18 19:18:37 +0300

    Updates

---
+++ precache-manifest.94cca300f1b724045f63eb1c77ef712e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44fe9d1dec7f30f5a4021b96ed488baa",
+    "url": "/roboticx/index.html"
+  },
+  {
+    "revision": "a8285c27abbec8864741",
+    "url": "/roboticx/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "7665038d04968b57d2de",
+    "url": "/roboticx/static/css/main.654d6a0e.chunk.css"
+  },
+  {
+    "revision": "a8285c27abbec8864741",
+    "url": "/roboticx/static/js/2.7ff329c8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/roboticx/static/js/2.7ff329c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7665038d04968b57d2de",
+    "url": "/roboticx/static/js/main.17b1f199.chunk.js"
+  },
+  {
+    "revision": "b00b1b0d7642d1bb352a",
+    "url": "/roboticx/static/js/runtime-main.2c8d0174.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/roboticx/static/media/SEGA.12a3db8b.woff"
+  }
+]);
